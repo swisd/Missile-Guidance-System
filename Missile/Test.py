94ff1b9fa951e109ff59ea419473537c3f9ef221
swisd <119827347+swisd@users.noreply.github.com>
2024-01-22 20:25:21 -0500

    Delete Missile directory

--- Missile/Test.py
+++
@@ -1,7 +0,0 @@
-"""Testing File"""
-import os
-from Missile.res import corrective_commands
-print(corrective_commands.Command.go_to_thr(corrective_commands.Command.command('command', 'cmd[x]'), 4, 100, 4))
-
-print("Current Working Directory " , str(os.getcwd()))
-print("Current File Directory " , str(os.getcwd()) + "/system")
