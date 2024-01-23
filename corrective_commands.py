c3c5d04fbafd54527b3dd6c0c1e60fa2a8605598
swisd <119827347+swisd@users.noreply.github.com>
2024-01-22 20:53:24 -0500

    Fix whitespace

---
+++ corrective_commands.py
@@ -0,0 +1,60 @@
+# corrective_commands.py
+"""Corrective Guidance Commands"""
+
+
+class Command:
+    """Corrective Commands"""
+    @staticmethod
+    def command(cmd: object = str) -> object:
+        """
+
+        :param cmd:
+        :return:
+        """
+        return str(cmd)
+    def go_to_pos(self, cpos: object, pos_x: object, pos_y: object, pos_z: object) -> object:
+        """
+
+        :param cpos:
+        :rtype: object
+        :param pos_x:
+        :param pos_y:
+        :param pos_z:
+        """
+    @staticmethod
+    def go_to_thr(t_c: object, t_t: object, t_cur: object) -> object:
+        """
+
+        :rtype: object
+        """
+        t_cs = t_c + (t_t - t_c) - t_cur
+        return t_cs
+    def __rtg_kt_co_vt_dp__(self, v_t: object, v_r: object, a_c: object, ) -> object:
+        """
+        __RTG_KT_CO_VT_DP__
+        :rtype: object
+        :param v_t:
+        :param v_r:
+        :param a_c:
+        """
+
+class Action:
+    """Control Actions"""
+    def pitch(self):
+        """
+        Pitch Control
+        """
+    def roll(self):
+        """
+        Roll Control
+        """
+    def yaw(self):
+        """
+        Yaw Control
+        """
+
+
+def ns_t():
+    """
+    Unknown Function
+    """
