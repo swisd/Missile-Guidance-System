84c276fdbb2301285d92090e185e4f8b513e9d83
swisd <119827347+swisd@users.noreply.github.com>
2024-01-26 14:20:43 -0500

    Delete Missile directory

--- Missile/res/ns_cwd.py
+++
@@ -1,9 +0,0 @@
-"""CWD File Handler"""
-import os
-
-
-def current_relative(internal):
-    """Current working directory relative to the location of implementation"""
-    cwd = str(os.getcwd())
-    relative_cwd = cwd + internal
-    return relative_cwd
